2d83c02286691cc00dcf95cca180fa4bf904fdfa
Bulat Sitdikov <rdvad2@gmail.com>
2022-11-05 21:18:41 +0300

    feat: update createUser controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const { ErrorCode, NotFound, DefaultError } = require('../errors/errors');
 
@@ -28,16 +29,19 @@ const getUser = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const { name, about, avatar, email, password } = req.body;
 
-  User.create({ name, about, avatar })
-    .then((user) => res.status(201).send(user))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(ErrorCode).send({ message: err.message });
-        return;
-      }
-      res.status(DefaultError).send({ message: 'Ошибка сервера.' });
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({ name, about, avatar, email, password: hash })
+        .then((user) => res.status(201).send(user))
+        .catch((err) => {
+          if (err.name === 'ValidationError') {
+            res.status(ErrorCode).send({ message: err.message });
+            return;
+          }
+          res.status(DefaultError).send({ message: 'Ошибка сервера.' });
+        });
     });
 };
 
--- models/user.js
+++ models/user.js
@@ -6,19 +6,19 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      default: 'Жак-Ив Кусто'б
       minlength: [2, '<2'],
       maxlength: [30, '>30'],
     },
     about: {
       type: String,
-      required: true,
+      default: 'Исследователь',
       minlength: [2, '<2'],
       maxlength: [30, '>30'],
     },
     avatar: {
       type: String,
-      required: true,
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     },
     email: {
       type: String,
