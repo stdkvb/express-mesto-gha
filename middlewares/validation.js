3060179ec2fac253d9232774506168ed67c974a8
Bulat Sitdikov <rdvad2@gmail.com>
2022-11-11 18:49:07 +0300

    ref: update readme.md

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -6,7 +6,7 @@ const linkRegEx = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()
 const createUserValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(linkRegEx),
